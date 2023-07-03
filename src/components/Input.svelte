fe5bba45af5d5bf5c8201c0bdf707335ddee8fd9
EdwardMelendezM <192666@unsaac.edu.pe>
2023-07-02 23:20:49 -0500

    create proyect

---
+++ src/components/Input.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let addTodo;
+  export let inputValue = ''
+  const onSubmit=(e)=>{
+    e.preventDefault()
+    if(inputValue.trim().length < 1){
+      return;
+    }
+    let newTodo = {
+      id:crypto.randomUUID(),
+      content:inputValue
+    }
+    addTodo(newTodo)
+    inputValue=''
+    
+  }
+</script>
+
+<div>
+  <form on:submit={onSubmit}>
+    <input
+    type="text"
+    bind:value={inputValue}
+    >
+    <button type="submit">Agregar</button>
+  </form>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	import Input from '../components/Input.svelte'
+	let listTodo=[]
+	const addTodo=(newValue)=>{
+		listTodo=[...listTodo, newValue]
+	}
+	$:console.log(listTodo)
+</script>
+
+<svelte:head>
+	<title>Todo</title>
+	<meta name="Todo application" content="Todo Svelte v4.0" />
+</svelte:head>
+
+<div class="container">
+	<div class="container_todo">
+		<h2>TODO APLICATION</h2>
+		<Input addTodo={addTodo}/>
+		<div class="container_todos">
+				{#each listTodo as todo (todo.id)}
+					<div class="container_todos_todo">{todo.content}</div>
+				{/each}
+		</div>
+	</div>
+</div>
+
+<style>
+	.container{
+		height: 100vh;
+		width: 100%;
+		display: flex;
+		align-items: flex-start;
+		justify-content: center;
+		background-color: #000;
+	}
+	.container_todo{
+		background-color: rgba(255,255,255,0.8);
+		max-width: 350px;
+		width: auto;
+		border-radius: 10px;
+		padding: 16px;
+		margin-top: 61px;
+	}
+</style>
